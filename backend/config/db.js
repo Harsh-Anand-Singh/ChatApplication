02e85f0677fe994c012249adb47708e54cbb0ef0
Harsh-Anand-Singh <130173825+Harsh-Anand-Singh@users.noreply.github.com>
2023-09-28 13:36:39 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/Chat", {
+    const conn = await mongoose.connect("mongodb+srv://harsh786anand:<Qwerty12345.>@chat.oskeaqk.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
